96f9c4277c824f04e8120c15091cba295ef8db0e
aankit <aankit.patel@gmail.com>
2015-02-21 18:26:19 -0500

    created the map, early game mechanics

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 // var titleScreen();
-
 var Game = function(familyNames){
 	//how big is the game, this will be important to creating
 	//a balanced world
 	var squareWidth = 20;
-	this.boardSize = displayWidth/squareWidth * displayHeight/squareWidth;
+	this.boardX = displayWidth/squareWidth;
+	this.boardY = displayHeight/squareWidth;
+	this.boardSize = this.boardX *  this.boardY;
 	this.board = new Array(this.boardSize);
 	this.families = [];
 	strokeWeight(0.2);
@@ -34,48 +35,80 @@ var Game = function(familyNames){
 		freshWater: {
 			p: 0.02,
 			c: color(138, 207, 237),
-			n: "Fresh Water"
+			n: "Fresh Water",
+			s: 0
 		},
 		saltWater: {
 			p: 0.70,
 			c: color(15, 111, 171),
-			n: "Salt Water"
+			n: "Salt Water",
+			s: 0
 		},
 		cropLand: {
 			p: 0.03,
 			c: color(133, 105, 45),
-			n: "Crop Land"
+			n: "Crop Land",
+			s: 0
 		},
 		pasture: {
 			p: 0.07,
 			c: color(45, 133, 51),
-			n: "Pasture"
+			n: "Pasture",
+			s: 0
 		},
 		forest: {
-			p: 0.07,
+			p: 0.06,
 			c: color(12, 99, 18),
-			n: "Forest"
+			n: "Forest",
+			s: 0
 		},
 		desert: {
-			p: 0.07,
+			p: 0.06,
 			c: color(219, 215, 77),
-			n: "Desert"
+			n: "Desert",
+			s: 0
 		},
 		mountain: {
-			p: 0.07,
+			p: 0.06,
 			c: color(99, 99, 99),
-			n: "Mountain"
+			n: "Mountain",
+			s: 0
 		}
 	};
 	//populate the world with resouces
+	var lastSquare = 0;
 	for (var resource in this.world){
 		if (this.world.hasOwnProperty(resource)){
 			n = Math.floor(this.world[resource].p * this.boardSize);
-			for 
+			startingSquare = lastSquare;
+			for(i=startingSquare;i<(startingSquare+n);i++){
+				this.board[i]=this.world[resource];
+				lastSquare ++;
+			}
 		}
 	}
-
+	// console.log(this.board.indexOf("Crop Land"))
+	this.board = shuffle(this.board);
+	for (var i in this.board){
+		try {
+			colortoUse = this.board[i].c;
+		}
+		catch (err){
+			this.board[i]=this.world.cropLand;
+		}
+		fill(this.board[i].c);
+		var y = Math.floor(i/this.boardX)*squareWidth;
+		console.log(y);
+		rect((i%this.boardX)*squareWidth,y,squareWidth, squareWidth);
+	}
 };
+
+
+
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
 
 var Actor = function(age, lifespan, links){
   //this.uid = uid;
